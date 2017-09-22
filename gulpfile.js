00d7b691c050e5f21eca4a5e11ccbfd8af1a0be0
David Okunlola <nybryx@yahoo.co.uk>
2017-09-22 23:22:23 +0100

    Update css ref | Update gulp css build

--- gulpfile.js
+++ gulpfile.js
@@ -69,6 +69,7 @@ gulp.task('browserify', function() {
 
 gulp.task('browserifyDist', function() {
     gulp.src(['app/js/main.ts'])
+        .pipe(babel())
         .pipe(browserify({
             insertGlobals: true,
             debug: true
@@ -86,37 +87,39 @@ gulp.task('less', function() {
 
 gulp.task('process-css', function() {
     var opts = { comments: true, spare: true };
-    gulp.src(['./app/css/main.css', '!./app/bower_components/**'])
+    gulp.src('./app/css/main.less')
+        .pipe(less())
         .pipe(minifyCSS())
         .pipe(autoprefixer({
             browsers: ['last 2 versions'],
             cascade: false
         }))
-        .pipe(concat('all.css'))
+        .pipe(concat('main.css'))
         .pipe(gulp.dest('./app/css/'))
 });
 
 gulp.task('process-css-dist', function() {
     var opts = { comments: true, spare: true };
-    gulp.src(['./app/css/main.css', '!./app/bower_components/**'])
+    gulp.src('./app/css/main.less')
+        .pipe(less())
         .pipe(minifyCSS())
         .pipe(autoprefixer({
             browsers: ['last 2 versions'],
             cascade: false
         }))
-        .pipe(concat('all.css'))
+        .pipe(concat('main.css'))
         .pipe(gulp.dest('./dist/css/'))
 });
 
 /*=====TASKS=====*/
 gulp.task('default', function() {
     runSequence(
-        ['clean'], ['less', 'process-css', 'connect', 'watch', 'browserify']
+        ['clean'], ['process-css', 'connect', 'watch', 'browserify']
     );
 });
 
 gulp.task('build', function() {
     runSequence(
-        ['clean'], ['less', 'copy-html-files', 'process-css-dist', 'copy-bower-components', 'connectDist', 'browserifyDist']
+        ['clean'], [ 'process-css-dist', 'copy-html-files', 'copy-bower-components', 'connectDist', 'browserifyDist']
     );
 });
