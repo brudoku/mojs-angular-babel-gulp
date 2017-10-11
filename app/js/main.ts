f1b54389e975c06f53b585e1961f06692c317337
David Okunlola <nybryx@yahoo.co.uk>
2017-10-11 20:41:30 +0100

    Add tween editor and player

--- app/js/main.ts
+++ app/js/main.ts
@@ -6,17 +6,22 @@ function log (msg){
 }
 const easingStep = new mojs.easing.path('M0, 100 C0, 100 10, 100 10, 100 C10, 100 10, 90 10, 90 C10, 90 25, 90 25, 90 C25, 90 25, 75 25, 75 C25, 75 40, 75 40, 75 C40, 75 40, 60 40, 60 C40, 60 50, 60 50, 60 C50, 60 50, 50 50, 50 C50, 50 60, 50 60, 50 C60, 50 60, 40 60, 40 C60, 40 65, 40 65, 40 C65, 40 65, 35 65, 35 C65, 35 70, 35 70, 35 C70, 35 100, 0 100, 0 ');
 const easingHill = new mojs.easing.path('M0, 100 C0, 100 1.898474554477918, 26.24438258837924 22.571428571428577, 45 C43.24438258837925, 63.75561741162074 40.40319341501881, 37.723531589552444 75, 45 C88.45394944212404, 93.41932555330472 100, 0 100, 0 ');
-
+const shiftCurve = mojs.easing.path( 'M0,100 C50,100 50,100 50,50 C50,0 50,0 100,0' );
+const scaleCurve = mojs.easing.path( 'M0,100 C21.3776817,95.8051376 50,77.3262711 50,0 C50,80.1708527 76.6222458,93.9449005 100,100' );
 const LETTER_OPTS = {
   opacity: {0:1},
   scale: {5: 1},
+  top: 0, left: 0,
+  // parent: '.mojs-line-tl-hook'
+  parent: '#sv-logo'
+  
 };
 
 const b = new mojs.Html({
   ...LETTER_OPTS,
-  x: {'-100':15},
+  x: {'-100':150},
   el: '#l-1',
-  y: {0: 100, },  
+  y: 100,
 });
 
 const r = new mojs.Html({
@@ -46,55 +51,30 @@ const logobru = new mojs.Html({
   
 });
 
-class Heart extends mojs.CustomShape {
-  getShape () { return '<path d="M92.5939814,7.35914503 C82.6692916,-2.45304834 66.6322927,-2.45304834 56.7076029,7.35914503 L52.3452392,11.6965095 C51.0327802,12.9714696 48.9328458,12.9839693 47.6203869,11.6715103 L47.6203869,11.6715103 L43.2705228,7.35914503 C33.3833318,-2.45304834 17.3213337,-2.45304834 7.43414268,7.35914503 C-2.47804756,17.1963376 -2.47804756,33.12084 7.43414268,42.9205337 L29.7959439,65.11984 C29.7959439,65.1323396 29.8084435,65.1323396 29.8084435,65.1448392 L43.2580232,78.4819224 C46.9704072,82.1818068 52.9952189,82.1818068 56.7076029,78.4819224 L70.1696822,65.1448392 C70.1696822,65.1448392 70.1696822,65.1323396 70.1821818,65.1323396 L92.5939814,42.9205337 C102.468673,33.12084 102.468673,17.1963376 92.5939814,7.35914503 L92.5939814,7.35914503 Z"></path>'; }
-  getLength () { return 292.110107421875; } // optional
-}
-
-mojs.addShape( 'heart', Heart ); 
-
-const heart = new mojs.Shape({
-  shape:    'heart',
-  fill:     'none',
-  stroke:   'white',
-  strokeDasharray: '100%',
-  strokeDashoffset: { '-100%' : '100%' },
-  y:         -20,
-  duration:  1000,
+const svlogo = new mojs.Html({
+  el: '#sv-logo', 
+  0: '50%',
+  duration: 300,
+  delay: 100,
+  easing: 'cubic.out',
 });
 
-var square = document.querySelector('#js-square');
+// class Heart extends mojs.CustomShape {
+//   getShape () { return '<path d="M92.5939814,7.35914503 C82.6692916,-2.45304834 66.6322927,-2.45304834 56.7076029,7.35914503 L52.3452392,11.6965095 C51.0327802,12.9714696 48.9328458,12.9839693 47.6203869,11.6715103 L47.6203869,11.6715103 L43.2705228,7.35914503 C33.3833318,-2.45304834 17.3213337,-2.45304834 7.43414268,7.35914503 C-2.47804756,17.1963376 -2.47804756,33.12084 7.43414268,42.9205337 L29.7959439,65.11984 C29.7959439,65.1323396 29.8084435,65.1323396 29.8084435,65.1448392 L43.2580232,78.4819224 C46.9704072,82.1818068 52.9952189,82.1818068 56.7076029,78.4819224 L70.1696822,65.1448392 C70.1696822,65.1448392 70.1696822,65.1323396 70.1821818,65.1323396 L92.5939814,42.9205337 C102.468673,33.12084 102.468673,17.1963376 92.5939814,7.35914503 L92.5939814,7.35914503 Z"></path>'; }
+//   getLength () { return 292.110107421875; } // optional
+// }
 
-var anim = new mojs.Html({
-  el: square,
-   y: {
-    0: '50%',
-    duration: 300,
-    delay: 100,
-    easing: 'cubic.out',
-  },
-  x: {
-    0: '50%',
-    duration: 300,
-    delay: 300,
-    easing: 'cubic.out',
-  },
-  width:{
-    '80%': '50%',
-    duration: 300,
-    delay: 600,
-    easing: 'cubic.out',
-  }
-});
+// mojs.addShape( 'heart', Heart ); 
 
-let LINE_OPTS = {
-  shape: 'line',  
-  stroke: 'white',
-  duration: 1000,
-  // strokeDashoffset: { '100%': '200%' }, // animate from 100% length to 200% length => shift from invisible to visible
-  strokeDashoffset: { '-100%': '100%' }, // alternatively  
-  strokeDasharray: {'100%':'100%'}, // make dash on the line 100% length visible followed by 100% length of transparent  
-};
+// const heart = new mojs.Shape({
+//   shape:    'heart',
+//   fill:     'none',
+//   stroke:   'white',
+//   strokeDasharray: '100%',
+//   strokeDashoffset: { '-100%' : '100%' },
+//   y:         -20,
+//   duration:  1000,
+// });
 
 /*@todo 
   calc dimensions and assign line xy values
@@ -132,6 +112,16 @@ let tuneLines = () => {
 let boxWidth = $('#main').width();
 let boxHeight = $('#main').height();
 
+let LINE_OPTS = {
+  shape: 'line',  
+  stroke: 'white',
+  duration: 1000,
+  opacity: {0:1},
+  // strokeDashoffset: { '100%': '200%' }, 
+  strokeDashoffset: { '-100%': '100%', easing: shiftCurve }, 
+  strokeDasharray: {'0.1%':'100%', easing: shiftCurve}, 
+};
+
 let lineTopLeft = new mojs.Shape({
   ...LINE_OPTS,
   parent: '.mojs-line-tl-hook',    
@@ -160,7 +150,7 @@ let lineBottomLeft = new mojs.Shape({
   parent: '.mojs-line-tl-hook',    
   angle: '-270',
   radius: boxHeight / 2,
-  delay: 750,
+  delay: 250,
 });
 let linesArray= [  
   lineTopLeft,
@@ -172,8 +162,7 @@ let linesArray= [
 const timeline = new mojs.Timeline();
 timeline.add([b, r, u, 
   logobru,
-   anim,
-   heart
+  //  heart
   ]);
 timeline.add(linesArray);
 tuneLines();
@@ -181,7 +170,7 @@ tuneLines();
 timeline.play();
 
 const RAD = 320;
-const SF = 0.8;
+const SF = 0.4;
 
 function rescale(){
   let w = util.getWindowSize();  
@@ -192,10 +181,6 @@ function tuneEm(arr){
   let w = util.getWindowSize();
   let rad = Math.abs(( w / RAD ) * SF) * 50;
   line.tune({radius: rad});
-  /* linesArray
-  _.each(arr, function(shape){
-    shape.tune({scale: rescale()});
-  }); */
 
   _.each(arr, function(shape){
     shape.tune({scale: rescale()});
@@ -214,14 +199,16 @@ const COLORS = {
 };
 const blendMode = 'screen';
 
-const y = -50;
+const y = 250;
 
 const staticTriangle = new mojs.Shape({
   shape:        'polygon',
+  points: 3,
   duration:     1160,
+  parent: '#sv-logo',
   
   radius:       { 60: 65 },
-  angle:       -60,
+  // angle:       -60,
   fill:         'none',
   stroke:       COLORS.white,
   strokeWidth:  { 30 : 5 },
@@ -229,6 +216,8 @@ const staticTriangle = new mojs.Shape({
   isShowEnd:    false,
   width:        170,
   height:       170,
+  angle: {'-180':0},
+  
   y
   });
 
@@ -236,9 +225,11 @@ const staticTriangle = new mojs.Shape({
 let shift1 = 87,
   shift2 = 50,
   SMALL_OPTS = {
-  scale: 3,
+  scale: {3 : 10},
   parent: '#sv-logo',
   shape:        'polygon',
+  points: 3,
+  
   duration:     1160,
   radius:       14,
   angle:       -60,
@@ -247,7 +238,7 @@ let shift1 = 87,
   strokeWidth:  { 14 : 4 },
   easing:       'expo.out',
   isShowEnd:    false,
-  // angle: {0:730}
+  angle: {'-180':0}
   };
 
 let small1 = new mojs.Shape({
@@ -269,12 +260,15 @@ let small3 = new mojs.Shape({
   });
 
 let SUPP_OPTS = {
-  scale: 3,
+  // scale: 3,
   parent: '#sv-logo',
   shape:        'polygon',
+  points: 3,
+  scale: {1: 8},
   duration:     1000,
   radius:       { 40: 20 },
-  angle:       -60,
+  angle:{'-180':0},
+  
   fill:         'white',
   fillOpacity:  { 0: 1 },
   stroke:       COLORS.white,
@@ -309,24 +303,35 @@ const line = new mojs.Shape({
   y: { 0: util.getScreenY() },
   x: { 0: util.getScreenX() },  
   duration: 400,
-  // angle: {0:360},
 }).then({
   y: { 0: util.getScreenX() },
   x: { 0: util.getScreenY() },  
   duration: 400,
   onComplete(){
-    timeline.replayBackward()
+    timeline.playBackward();
   } 
 });
 
-var shapeArray = [staticTriangle, small1, small2, small3, support1, support2, line];
+let shapeArray = [staticTriangle, small1, small2, small3, support1, support2, line];
 
+const aCurve = new MojsCurveEditor({
+  name:         'aCurve'
+});  
 
+let square2 = new mojs.Shape({
+  shape: 'polygon',
+  points: 4,
+  fill: 'none',
+  stroke: COLORS.white,
+  scale: {2: 1, curve: aCurve.getEasing()  } 
+});
+
+const mojsPlayer = new MojsPlayer({ add: square2 });
 
 timeline
 .add(
 shapeArray, 
-
+square2
 );
 
 tuneEm(shapeArray)
