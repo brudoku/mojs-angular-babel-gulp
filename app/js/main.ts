08b32e7395d8047ebb54a7f20e54643d15e42044
David Okunlola <nybryx@yahoo.co.uk>
2017-10-12 01:59:26 +0100

    Animate lines

--- app/js/main.ts
+++ app/js/main.ts
@@ -1,19 +1,20 @@
 'use strict';
-const utilimport = require('./libs/util.ts');
-const util = new utilimport();
-function log (msg){
-  console.log(msg);
-  }
-const easingStep = new mojs.easing.path('M0, 100 C0, 100 10, 100 10, 100 C10, 100 10, 90 10, 90 C10, 90 25, 90 25, 90 C25, 90 25, 75 25, 75 C25, 75 40, 75 40, 75 C40, 75 40, 60 40, 60 C40, 60 50, 60 50, 60 C50, 60 50, 50 50, 50 C50, 50 60, 50 60, 50 C60, 50 60, 40 60, 40 C60, 40 65, 40 65, 40 C65, 40 65, 35 65, 35 C65, 35 70, 35 70, 35 C70, 35 100, 0 100, 0 ');
-const easingHill = new mojs.easing.path('M0, 100 C0, 100 1.898474554477918, 26.24438258837924 22.571428571428577, 45 C43.24438258837925, 63.75561741162074 40.40319341501881, 37.723531589552444 75, 45 C88.45394944212404, 93.41932555330472 100, 0 100, 0 ');
-const shiftCurve = mojs.easing.path( 'M0,100 C50,100 50,100 50,50 C50,0 50,0 100,0' );
-const scaleCurve = mojs.easing.path( 'M0,100 C21.3776817,95.8051376 50,77.3262711 50,0 C50,80.1708527 76.6222458,93.9449005 100,100' );
-const timeline = new mojs.Timeline();
+/*...*/
+  const utilimport = require('./libs/util.ts');
+  const util = new utilimport();
+  function log (msg){
+    console.log(msg);
+    }
+  const easingStep = new mojs.easing.path('M0, 100 C0, 100 10, 100 10, 100 C10, 100 10, 90 10, 90 C10, 90 25, 90 25, 90 C25, 90 25, 75 25, 75 C25, 75 40, 75 40, 75 C40, 75 40, 60 40, 60 C40, 60 50, 60 50, 60 C50, 60 50, 50 50, 50 C50, 50 60, 50 60, 50 C60, 50 60, 40 60, 40 C60, 40 65, 40 65, 40 C65, 40 65, 35 65, 35 C65, 35 70, 35 70, 35 C70, 35 100, 0 100, 0 ');
+  const easingHill = new mojs.easing.path('M0, 100 C0, 100 1.898474554477918, 26.24438258837924 22.571428571428577, 45 C43.24438258837925, 63.75561741162074 40.40319341501881, 37.723531589552444 75, 45 C88.45394944212404, 93.41932555330472 100, 0 100, 0 ');
+  const shiftCurve = mojs.easing.path( 'M0,100 C50,100 50,100 50,50 C50,0 50,0 100,0' );
+  const scaleCurve = mojs.easing.path( 'M0,100 C21.3776817,95.8051376 50,77.3262711 50,0 C50,80.1708527 76.6222458,93.9449005 100,100' );
+  const timeline = new mojs.Timeline();
 
 /*VARS*/
   const y = 250;
   const RAD = 320;
-  const SF = 0.4;
+  const SF = 0.2;
   const COLORS = {
     white:   '#ffffff',
     black:   '#000000',
@@ -24,10 +25,11 @@ const timeline = new mojs.Timeline();
     yellow:  '#FFE202',
     hotpink: 'deeppink',
     };
-  const aCurve = new MojsCurveEditor({name:'aCurve'});  
   let boxWidth = $('#main').width();
   let boxHeight = $('#main').height();
-
+  
+  const aCurve = new MojsCurveEditor({name:'bCurve'});  
+  
 
 /*TEXT*/
   const LETTER_OPTS = {
@@ -66,40 +68,48 @@ const timeline = new mojs.Timeline();
     shape: 'line',  
     parent: '.mojs-line-tl-hook',    
     stroke: 'white',
+    strokeWidth: 1,
     duration: 1000,
     opacity: {0:1},
     // strokeDashoffset: { '100%': '200%' }, 
     strokeDashoffset: { '-100%': '100%', easing: shiftCurve }, 
     strokeDasharray: {'0.1%':'100%', easing: shiftCurve}, 
+    // strokeDashoffset: { '-100%': '100%', curve: shiftCurve }, 
+    // strokeDasharray: {'0.1%':'100%', curve: shiftCurve}, 
     };
-
+let LINE_OPTS_THEN= {
+    delay: 500,
+    strokeWidth: 0.1,    
+    strokeDashoffset: { '100%': '-100%', curve: shiftCurve }, 
+    strokeDasharray: {'100%':'2%', curve: shiftCurve},   
+}
   let lineTopLeft = new mojs.Shape({
     ...LINE_OPTS,
     angle: 180,
     radius: boxWidth / 2,
     scale: 1
-    });
+    }).then(LINE_OPTS_THEN);
 
   let lineTopRight = new mojs.Shape({
     ...LINE_OPTS, 
     angle: 270,
     radius: boxHeight / 2,
     delay: 250,
-    });
+    }).then(LINE_OPTS_THEN);
 
   let lineBottomRight = new mojs.Shape({
     ...LINE_OPTS,
     angle: 0,
     radius: boxWidth / 2,
     delay: 300
-    });
+    }).then(LINE_OPTS_THEN);
 
   let lineBottomLeft = new mojs.Shape({
     ...LINE_OPTS, 
     angle: '-270',
     radius: boxHeight / 2,
     delay: 250,
-    });
+    }).then(LINE_OPTS_THEN);
 
   let tuneLines = () => {
     let topLeft = $('.mojs-line-tl-hook').position();
@@ -124,18 +134,11 @@ const timeline = new mojs.Timeline();
     })
 
     lineBottomLeft.tune({
-      x: topLeft.left,
+      x: topLeft.left + 15,
       y: boxHeight/2,
       radius: boxHeight,    
     })  
     }
-  let rescale = () => {
-    let w = util.getWindowSize();  
-    return Math.abs(( w / RAD ) * SF);
-    }
-
-
-
 
 
 /*SQUARE*/
@@ -144,50 +147,73 @@ const timeline = new mojs.Timeline();
     points: 4,
     fill: 'none',
     stroke: COLORS.white,
-    // scale: {2: 1, curve: aCurve.getEasing()  } 
+    duration: 1000,
+    radius: {0: RAD, curve: 'cubic.in' } ,
+    opacity: {1:0, curve: aCurve.getEasing()},
+    strokeWidth: 1,
+    
+    // strokeWidth: {1:4, curve: aCurve.getEasing()}
+    })/* .then({
+      delay: 250,
+      angle: {0:45}
+    }) */
+
+  let cross = new mojs.Shape({
+    shape: 'cross',
+    fill: 'none',
+    stroke: COLORS.white,    
+    radius: {0: RAD, curve: 'cubic.in' } ,
+    opacity: {1:0}
     });
 
 /*FUNC*/
-  let tuneEm = (arr) => {
-    let w = util.getWindowSize();
-    let rad = Math.abs(( w / RAD ) * SF) * 50;
-    _.each(arr, function(shape){
-      shape.tune({scale: rescale()});
-    });
+  let rescale = () => {
+    let w = util.getWindowSize();  
+    return Math.abs(( w / RAD ) * SF);
     }
+  let tuneShapes = (arr) => {
+      let w = util.getWindowSize();
+      let rad = Math.abs(( w / RAD ) * SF) * 50;
+      _.each(arr, function(shape){
+        shape.tune({scale: rescale()});
+        // shape.tune({radius: rescale()*100});
+        
+      });
+      }
 /*TLINE*/
-  let linesArray= [  
+  timeline.add(
+    letter1, 
+    letter2, 
+    letter3,
+  );
+  
+  let linesArray = [  
     lineTopLeft,
     lineTopRight,
     lineBottomRight,
     lineBottomLeft,
     ];
     
-    let shapeArray = [square2];
+  let shapeArray = [
+    square2,
+    cross,
+  ];
+  
+  timeline.add(linesArray);
 
-  timeline.add(
-    letter1, 
-    letter2, 
-    letter3,
-    shapeArray, 
-    );
-
-  timeline.add(
-      linesArray,
-    )    
+  timeline.add(shapeArray);
 
   tuneLines();
 
-  tuneEm(shapeArray)
+  tuneShapes(shapeArray)
 
   timeline.play();
-
 
 /*EDIT*/
   const mojsPlayer = new MojsPlayer({ add: square2 });
 
   window.addEventListener('resize', function(e){
-      tuneEm(shapeArray);
+      tuneShapes(shapeArray);
       tuneLines();
     });
 
