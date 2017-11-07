1c32133d48e1b92d1762189ced590fd51ba77cb1
David Okunlola <nybryx@yahoo.co.uk>
2017-11-07 23:53:04 +0000

    align mojs line on hover

--- app/js/directives/linkAnim.ts
+++ app/js/directives/linkAnim.ts
@@ -12,25 +12,28 @@ let linkAnim = () => {
             
             let $elem = $(elem);
             const shiftCurve = mojs.easing.path( 'M0,100 C50,100 50,100 50,50 C50,0 50,0 100,0' );
-            const scaleCurve = mojs.easing.path( 'M0,100 C21.3776817,95.8051376 50,77.3262711 50,0 C50,80.1708527 76.6222458,93.9449005 100,100' );            
+            const scaleCurve = mojs.easing.path( 'M0,100 C21.3776817,95.8051376 50,77.3262711 50,0 C50,80.1708527 76.6222458,93.9449005 100,0' );            
             const line = new mojs.Shape({
                 shape:        'line',
-                top:          boxHeight/2,
+                top:          -($elem.height()/1.1),
                 parent:       $elem.parent(),
                 stroke: '#000',
                 strokeWidth: 1,
                 duration: 1000,
                 opacity: { 0: 1 },                
                 angle: 0,
-                radius: $elem.width(),
-                strokeDashoffset: { '-100%': '100%', easing: shiftCurve },
-                strokeDasharray: { '0.1%': '100%', easing: shiftCurve },                  
+                radius: {0:$elem.width(), easing:mojs.easing.cubic.out},
+                // strokeDashoffset: { '-100%': '100%', easing: shiftCurve },
+                // strokeDasharray: { '0.1%': '100%', easing: shiftCurve },                  
               });
-              $elem.mouseover(()=>{
-                line
-                .tune({ y: $elem.position().top + 20 })
-                .replay();
-              })
+            $elem.mouseover(()=>{
+            line
+            .tune({ y: $elem.position().top + 20 })
+            .replay();
+            });
+            $elem.mouseout(() => {
+            line.playBackward();
+            });              
         }
     }
 }
