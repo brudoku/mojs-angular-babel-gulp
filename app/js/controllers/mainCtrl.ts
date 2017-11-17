95b27c1216cd1da5f3d5802ec17b136feffdc409
David Okunlola <nybryx@yahoo.co.uk>
2017-11-17 16:43:19 +0000

    animate changing tracks | remove unused fn call

--- app/js/controllers/mainCtrl.ts
+++ app/js/controllers/mainCtrl.ts
@@ -4,8 +4,6 @@ let mainCtrl = function($scope, $rootScope, $state, $timeout, lazyService, Utili
         console.log(response);
     });
 
-    Utility.foo();
-
     $scope.pages = ["listen", "bio", "media"];
     $scope.currentPage = "";
 
