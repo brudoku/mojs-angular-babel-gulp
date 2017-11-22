cb73ff9ff1e882b405497e6b3d11a805c15d2686
David Okunlola <nybryx@yahoo.co.uk>
2017-11-22 01:31:19 +0000

    animate player on page load

--- app/js/directives/pageLoadAnim.ts
+++ app/js/directives/pageLoadAnim.ts
@@ -1,19 +1,37 @@
 "use strict";
 
 let pageLoadAnim = ($timeout, $rootScope, $animate) => {
+    function getRandomInt(min, max) {
+      return Math.floor(Math.random() * (max - min + 1)) + min;
+    }      
     return {
         restrict: 'A',
         scope: false,
         link: (scope, elem, attrs) => {
             var $el = $(elem)
-      
             console.log($el);
             
              $rootScope.$on("$viewContentLoaded", function(){
-                 $timeout(function () {
-                     console.log("$viewContentLoaded=========");
-                      $animate.addClass($el, 'player-active');
-                 },2000)
+                $el.snabbt({
+                delay: 500,
+                easing: "ease",
+                opacity: 1,
+                fromOpacity: 0,
+                duration: 1000,
+                background: '#000',
+                fromRotation: [0, 0, 0],
+                rotation: [0, 0, 0],
+                fromPosition: function() {
+                    return _.map(
+                        _.range(3, function(num) {
+                            return getRandomInt(100, 300);
+                        })
+                    );
+                },
+                position: [0, 0, 0],
+                fromScale: [5, 5],
+                scale: [1, 1]
+                });
              }); 
         }
     }
