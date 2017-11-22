91f595a31a6e0fc94e59e0576d3bbd9ece968f10
David Okunlola <nybryx@yahoo.co.uk>
2017-11-22 21:37:01 +0000

    player load anim edit

--- app/js/directives/pageLoadAnim.ts
+++ app/js/directives/pageLoadAnim.ts
@@ -1,38 +1,27 @@
 "use strict";
 
 let pageLoadAnim = ($timeout, $rootScope, $animate) => {
-    function getRandomInt(min, max) {
-      return Math.floor(Math.random() * (max - min + 1)) + min;
-    }      
     return {
         restrict: 'A',
         scope: false,
         link: (scope, elem, attrs) => {
             var $el = $(elem)
-            console.log($el);
-            
-             $rootScope.$on("$viewContentLoaded", function(){
-                $el.snabbt({
-                delay: 500,
-                easing: "ease",
-                opacity: 1,
-                fromOpacity: 0,
-                duration: 1000,
-                background: '#000',
-                fromRotation: [0, 0, 0],
-                rotation: [0, 0, 0],
-                fromPosition: function() {
-                    return _.map(
-                        _.range(3, function(num) {
-                            return getRandomInt(100, 300);
-                        })
-                    );
-                },
-                position: [0, 0, 0],
-                fromScale: [5, 5],
-                scale: [1, 1]
-                });
-             }); 
+            $el.snabbt({
+            delay: 1000,
+            easing: "ease",
+            opacity: 1,
+            fromOpacity: 0,
+            duration: 2000,
+            background: '#000',
+            fromRotation: [0, 0, 0],
+            rotation: [0, 0, 0],
+            fromPosition: [0,0,0],
+            position: [0, 0, 0],
+            fromScale: [3, 3],
+            scale: [1, 1]
+            });
+            $rootScope.$on("$viewContentLoaded", function(){
+            }); 
         }
     }
 }
