dbd2135216211fde19afb1fc1575a8c52eb5b83b
David Okunlola <nybryx@yahoo.co.uk>
2017-11-22 21:37:20 +0000

    animate waveform on track selection

---
+++ app/js/animations/waveformAnim.ts
@@ -0,0 +1,41 @@
+'use strict';
+
+var waveAnim = function($timeout) {
+  return {
+    enter: function(element, done) {
+      var $el = $(element);
+      $el.snabbt({
+        easing: "ease",
+        opacity: 1,
+        fromOpacity: 0,
+        duration: 1000,
+        fromPosition: [0, 100, 0],
+        position: [0, 0, 0]
+        // fromScale: [5,5],
+        // scale: [1,1],
+      });
+      $timeout(function() {
+        $el.css("transform", "none");
+        done();
+      }, 1000);
+    },
+    leave: function(element, done) {
+      var $el = $(element);
+      console.log("leave");
+      $el.snabbt({
+        easing: "ease",
+        fromOpacity: 1,
+        opacity: 0,
+        duration: 1000,
+        fromPosition: [0, 0, 0],
+        position: [0, -100, 0]
+      });
+      done();
+      // $timeout(function() {
+      //   $el.css("transform", "none");
+      // }, 1000);
+    }
+  };
+};
+
+module.exports = waveAnim;
