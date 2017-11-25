121f85487cbe46bf21aba51e33718fd0ed38609e
David Okunlola <nybryx@yahoo.co.uk>
2017-11-25 01:12:10 +0000

    prev next bt anim

--- app/js/directives/navAnim.ts
+++ app/js/directives/navAnim.ts
@@ -3,26 +3,28 @@ let navAnim = ($timeout, $rootScope) => {
         restrict: 'A',
         scope: {},
         link: (scope, elem, attrs) => {
-            console.log(scope.$id);
             const timeline = new mojs.Timeline();
             
             const link1 = new mojs.Html({
                 scale: { 0: 1 },
-                delay: 1000,
-                el: "#l-1",
+                delay: 500,
+                duration: 750,
+                el: "#prev",
                 top: { "-200": 0 }
             });
             
             const link2 = new mojs.Html({
                 scale: { 0: 1 },
-                delay: 1000,
-                el: "#l-2",
+                delay: 750,
+                duration: 500,
+                el: "#next",
                 top: { "200": 0 }
             });
 
             timeline.add(link1, link2);
-            timeline.play();
-
+            $rootScope.$on("anim-player-loaded", function() {
+                timeline.play();
+            });             
         }
     }
 }
